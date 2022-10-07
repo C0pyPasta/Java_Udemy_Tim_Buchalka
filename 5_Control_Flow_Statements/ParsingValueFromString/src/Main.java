e0fb371b5db3d305399ef213d8becec22de5187d
C0pyPasta <114156085+C0pyPasta@users.noreply.github.com>
2022-10-07 11:21:55 +0200

    Update Main.java

--- 5_Control_Flow_Statements/ParsingValueFromString/src/Main.java
+++ 5_Control_Flow_Statements/ParsingValueFromString/src/Main.java
@@ -1,10 +1,11 @@
 public class Main {
     public static void main(String[] args) {
+        
         printSquareStar(300);
+        
     }
 
     public static void printSquareStar(int number) {
-
         if (number < 5) {
             System.out.println("Invalid Value");
         } else {
@@ -12,8 +13,10 @@ public class Main {
             int columnCount;
             int currentRow;
             rowCount = columnCount = number;
+            
             for (int row = 0; row < rowCount; row++) {
                 currentRow = row;
+                
                 for (int column = 0; column < columnCount; column++) {
                     if (row == 0 || column == 0 || row == rowCount - 1 || column == columnCount - 1 || column == row || column == (rowCount - currentRow - 1)) {
                         System.out.print("*");
