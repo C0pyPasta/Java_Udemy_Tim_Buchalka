f63377550d77c096959936eca2207713d6f713a6
C0pyPasta <114156085+C0pyPasta@users.noreply.github.com>
2022-10-07 12:26:38 +0200

    Update Main.java

--- 11_Naming_Conventions_and_Packages_Static_and_Final_Keywords/TheStaticStatement/src/Main.java
+++ 11_Naming_Conventions_and_Packages_Static_and_Final_Keywords/TheStaticStatement/src/Main.java
@@ -1,5 +1,4 @@
 public class Main {
-
     public static int multiplier = 7;
 
     public static void main(String[] args) {
@@ -16,7 +15,6 @@ public class Main {
         int answer = multiply(6);
         System.out.println("The answer is " + answer);
         System.out.println("Multiplier is " + multiplier);
-
 
     }
 
