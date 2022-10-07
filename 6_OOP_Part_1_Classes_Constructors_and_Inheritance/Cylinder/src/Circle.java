034b38538c084688678763b02bfca5660bb690d8
C0pyPasta <114156085+C0pyPasta@users.noreply.github.com>
2022-10-07 11:33:12 +0200

    Update Circle.java

--- 6_OOP_Part_1_Classes_Constructors_and_Inheritance/Cylinder/src/Circle.java
+++ 6_OOP_Part_1_Classes_Constructors_and_Inheritance/Cylinder/src/Circle.java
@@ -1,15 +1,13 @@
 public class Circle {
-    //Instance Variable
     private double radius;
 
-    //Constructor (Let op deze heeft een hoofdletter
     public Circle(double radius) {
         if(radius == 0.0) {
             this.radius = 0;
         }
         this.radius = radius;
     }
-    //Getter instance method
+
     public double getRadius() {
         if(this.radius <= 0) {
             return 0.0;
