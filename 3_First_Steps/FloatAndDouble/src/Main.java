16baf03ddfe98bf3aab236d40e43c37edac69b21
C0pyPasta <114156085+C0pyPasta@users.noreply.github.com>
2022-10-07 11:06:34 +0200

    Update Main.java

--- 3_First_Steps/FloatAndDouble/src/Main.java
+++ 3_First_Steps/FloatAndDouble/src/Main.java
@@ -13,17 +13,17 @@ public class Main {
         System.out.println("Double maximum value = " + myMaxDoubleValue);
 
         int myIntValue = 5 / 3;
-        float myFloatValue = 5f / 3f; // f voor float
-        double myDoubleValue = 5d / 3d; // d voor double
+        float myFloatValue = 5f / 3f; // f stands for float
+        double myDoubleValue = 5d / 3d; // d stands for double
         System.out.println("MyIntValue= " + myIntValue);
         System.out.println("MyFloatValue= " + myFloatValue);
         System.out.println("MyDoubleValue= " + myDoubleValue);
 
 
         //Challenge
-        float myFloatValueTest = (float) 5.25; // float gacast op double die default gegeven wordt door Java
+        float myFloatValueTest = (float) 5.25; // float cast on a double (default)
 
-        // double wordt meeste gebruikt, float niet meer zoveel
+        //double is mostly used
 
         //Challenge
         double myPounds = 200d;
@@ -31,7 +31,7 @@ public class Main {
         System.out.println(myPounds + " pound = " + myKilograms + " Kilograms");
 
         double pi = 3.1415927d;
-        double anotherNumber = 3_000_000.4_567_890d; //Underscore mag je als punt gebruiken voor leesbaarheid
+        double anotherNumber = 3_000_000.4_567_890d; //undescore instead of a . (dot) can be used
         System.out.println(pi);
         System.out.println(anotherNumber);
 
