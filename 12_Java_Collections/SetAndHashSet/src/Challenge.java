089d7e5dc634feb9efea4d407b7f2eca49fffa3e
C0pyPasta <114156085+C0pyPasta@users.noreply.github.com>
2022-10-07 10:56:31 +0200

    Add files via upload

---
+++ 12_Java_Collections/SetAndHashSet/src/Challenge.java
@@ -0,0 +1,21 @@
+public class Challenge {
+    /**
+     * When overriding the equals() method in the HeavenlyBody class, we
+     * were careful to make sure that it would net return true if a HeavenlyBody
+     * was compared to a subclass of itself.
+     *
+     * We did that to demonstrate that method, but it was actually
+     * unnecessary in the HeavenlyBody class.
+     *
+     * The mini challenge is just a question: why was it unnecessary?
+     *
+     * Hint: If you are stuck, check out Lecture 97
+     *
+     * Answer:
+     *
+     * The Heavenly class is declared final, so cannot be subclassed.
+     * The Java String class is also final, which is why it can safely
+     * use the instanceof method without having to worry about
+     * comparisons with a subclass.
+     */
+}
