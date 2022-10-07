0950fb61b26a42b5f9fcb507e49a8052280418cb
C0pyPasta <114156085+C0pyPasta@users.noreply.github.com>
2022-10-07 12:14:35 +0200

    Update ITelephone.java

--- 9_Inner_and_Abstract_Classes_and_Interfaces/Interfaces/src/ITelephone.java
+++ 9_Inner_and_Abstract_Classes_and_Interfaces/Interfaces/src/ITelephone.java
@@ -1,6 +1,6 @@
 public interface ITelephone {
-    public void powerOn(); // Hoeft geen access modifier (public) te hebben want een interface moet altijd geimplementeerd worden
-    void dial(int phoneNumber); // Hier access modifier weggelaten
+    public void powerOn(); // Doesn't need a public access modifier because it is an Interface and all methods need to be implemented
+    void dial(int phoneNumber);
     void answer();
     boolean callPhone(int phoneNumber);
     boolean isRinging();
