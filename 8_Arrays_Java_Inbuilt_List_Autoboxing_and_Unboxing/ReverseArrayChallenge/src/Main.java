9d2a059b203f162e12d4ef05212989d177cd17e4
C0pyPasta <114156085+C0pyPasta@users.noreply.github.com>
2022-10-07 12:03:18 +0200

    Update Main.java

--- 8_Arrays_Java_Inbuilt_List_Autoboxing_and_Unboxing/ReverseArrayChallenge/src/Main.java
+++ 8_Arrays_Java_Inbuilt_List_Autoboxing_and_Unboxing/ReverseArrayChallenge/src/Main.java
@@ -13,7 +13,6 @@ public class Main {
     }
 
     private static void reverse(int[] array){
-
         int maxIndex = array.length -1;
         int halfLength = array.length / 2;
         for(int i=0; i < halfLength; i++){
