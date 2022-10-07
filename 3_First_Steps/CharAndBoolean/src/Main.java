701732f87aa5d94f1469cff6eafed0f07c7b243a
C0pyPasta <114156085+C0pyPasta@users.noreply.github.com>
2022-10-07 11:03:23 +0200

    Update Main.java

--- 3_First_Steps/CharAndBoolean/src/Main.java
+++ 3_First_Steps/CharAndBoolean/src/Main.java
@@ -3,15 +3,15 @@ public class Main {
     public static void main(String[] args) {
 
         char myChar = 'D';
-        char myUnicodeChar = '\u0044'; // de "slash u" geeft aan dat het unicode is (unicode-table.com voor de unicodes)
+        char myUnicodeChar = '\u0044'; // \u stands for unicode -> unicode-table.com
         System.out.println(myChar);
         System.out.println(myUnicodeChar);
+        
         char myCopyrightChar = '\u00A9';
         System.out.println(myCopyrightChar);
+        
         boolean myTrueBooleanValue = true;
         boolean myFalseBooleanValue = false;
-
         boolean isCustomerOverTwentyOne = true;
-
     }
 }
