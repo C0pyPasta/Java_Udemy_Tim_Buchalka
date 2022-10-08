2c3a11aa793ae4dd4c3af58eca7ab27d9eb1fe65
C0pyPasta <114156085+C0pyPasta@users.noreply.github.com>
2022-10-08 18:44:54 +0200

    Update Location.java

--- 14_Basic_Input_and_Output_including_javaUtil/InputOutput/src/Location.java
+++ 14_Basic_Input_and_Output_including_javaUtil/InputOutput/src/Location.java
@@ -1,7 +1,6 @@
 import java.util.HashMap;
 import java.util.Map;
 
-
 public class Location {
     private final int locationID;
     private final String description;
@@ -10,11 +9,11 @@ public class Location {
     public Location(int locationID, String description, Map<String, Integer> exits) {
         this.locationID = locationID;
         this.description = description;
-        if(exits != null) {
-            this.exits = new HashMap<String, Integer>(exits);
-        } else {
-            this.exits = new HashMap<String, Integer>();
-        }
+            if(exits != null) {
+                this.exits = new HashMap<String, Integer>(exits);
+            } else {
+                this.exits = new HashMap<String, Integer>();
+            }
         this.exits.put("Q", 0);
     }
 
