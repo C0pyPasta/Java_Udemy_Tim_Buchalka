8fd08735b33f2a3e60737ef9bd2883521ca0f44a
C0pyPasta <114156085+C0pyPasta@users.noreply.github.com>
2022-10-13 08:39:30 +0200

    Add files via upload

---
+++ 14_Basic_Input_and_Output_including_javaUtil/Paths/src/Main.java
@@ -0,0 +1,91 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.nio.file.*;
+import java.nio.file.attribute.BasicFileAttributes;
+
+public class Main {
+    public static void main(String[] args) {
+        try {
+
+            // Normally you want te read/write to a file so a Stream or Channel would be used instead of a Path / Files combination
+//            Path fileToCreate = FileSystems.getDefault().getPath("Examples", "file2.txt");
+//            Files.createFile(fileToCreate);
+//            Path dirToCreate = FileSystems.getDefault().getPath("Examples", "Dir4");
+//            Files.createDirectory(dirToCreate);
+
+//            Path dirToCreate = FileSystems.getDefault().getPath("Examples", "Dir2/Dir3/Dir4/Dir5/Dir6");
+//            Files.createDirectories(dirToCreate);
+
+//            Path dirToCreate = FileSystems.getDefault().getPath("Examples/Dir2/Dir3/Dir4/Dir5/Dir6/Dir7");
+//            Files.createDirectories(dirToCreate);
+
+            Path filePath = FileSystems.getDefault().getPath("Examples", "Dir1/file1.txt");
+            long size = Files.size(filePath);
+            System.out.println("Size = " + size + " bytes");
+            System.out.println("Last modified = " + Files.getLastModifiedTime(filePath));
+
+            BasicFileAttributes attrs = Files.readAttributes(filePath, BasicFileAttributes.class);
+            System.out.println("Size = " + attrs.size());
+            System.out.println("Last modified = " + attrs.lastModifiedTime());
+            System.out.println("Created = " + attrs.creationTime());
+            System.out.println("Is directory = " + attrs.isDirectory());
+            System.out.println("Is regular file = " + attrs.isRegularFile());
+
+//            Path fileToDelete = FileSystems.getDefault().getPath("Examples", "Dir1", "file1copy.txt");
+//            Files.deleteIfExists(fileToDelete);
+
+//            Path fileToMove = FileSystems.getDefault().getPath("Examples", "file1.txt");
+//            Path destination = FileSystems.getDefault().getPath("Examples", "file1.txt");
+//            Files.move(fileToMove, destination);
+
+//            Path sourceFile = FileSystems.getDefault().getPath("Examples", "file1.txt");
+//            Path copyFile = FileSystems.getDefault().getPath("Examples", "file1copy.txt");
+//            Files.copy(sourceFile, copyFile, StandardCopyOption.REPLACE_EXISTING);
+
+//            sourceFile = FileSystems.getDefault().getPath("Examples", "Dir1");
+//            copyFile = FileSystems.getDefault().getPath("Examples", "Dir4");
+//            Files.copy(sourceFile, copyFile, StandardCopyOption.REPLACE_EXISTING);
+
+        } catch (IOException e) {
+            System.out.println(e.getMessage());
+        }
+    }
+//        Path path = FileSystems.getDefault().getPath("WorkingDirectoryFile.txt");
+//        printFile(path);
+////        Path filePath = FileSystems.getDefault().getPath("files","SubdirectoryFile.txt");
+//        Path filePath = Paths.get(".","files","SubdirectoryFile.txt");
+//        printFile(filePath);
+//        filePath = Paths.get("C:\\Users\\visie-groep\\Desktop\\RutgerJava\\14_Basic_Input_and_Output_including_JavaUtil\\OutThere.txt");
+//        printFile(filePath);
+//
+//        filePath = Paths.get(".");
+//        System.out.println(filePath.toAbsolutePath());
+//
+//        Path path2 = FileSystems.getDefault().getPath(".", "files", "..", "files", "SubdirectoryFile.txt");
+//        System.out.println(path2.normalize().toAbsolutePath());
+//        printFile(path2.normalize());
+//
+//        Path path3 = FileSystems.getDefault().getPath("thisFileDoesntExist.txt");
+//        System.out.println(path3.toAbsolutePath());
+//
+//        Path path4 = Paths.get("\\testing\\test\\", "abcdef", "nonExistingFile.txt");
+//        System.out.println(path4.toAbsolutePath());
+//
+//        filePath = FileSystems.getDefault().getPath("files");
+//        System.out.println("Exists = " + Files.exists(filePath));
+//
+//        System.out.println("Exists = " + Files.exists(path4) );
+//    }
+//
+//    private static void printFile(Path path){
+//        try(BufferedReader fileReader = Files.newBufferedReader(path)) {
+//            String line;
+//            while ((line = fileReader.readLine()) != null) {
+//                System.out.println(line);
+//            }
+//        } catch (IOException e) {
+//            System.out.println(e.getMessage());
+//            e.printStackTrace();
+//        }
+//    }
+}
