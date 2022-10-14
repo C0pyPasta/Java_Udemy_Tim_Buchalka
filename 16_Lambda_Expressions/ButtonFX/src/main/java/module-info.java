417b6eaccad8099a98f0330068b1ab7d385dc61a
C0pyPasta <114156085+C0pyPasta@users.noreply.github.com>
2022-10-14 12:05:46 +0200

    Add files via upload

---
+++ 16_Lambda_Expressions/ButtonFX/src/main/java/module-info.java
@@ -0,0 +1,9 @@
+module com.example.buttonfx {
+    requires javafx.controls;
+    requires javafx.fxml;
+
+    requires org.controlsfx.controls;
+
+    opens com.example.buttonfx to javafx.fxml;
+    exports com.example.buttonfx;
+}
